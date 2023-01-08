be631a5809c13a7a359b9529954b0e5a71839744
ruinaz90 <rzhao9002@gmail.com>
2023-01-08 06:32:13 -0500

    added Tailwind and Flowbite

--- src/components/ShowCard.jsx
+++ src/components/ShowCard.jsx
@@ -1,7 +1,7 @@
 function ShowCard({show}) {
     return (
-        <div>
-            <h2>{show.name}</h2>
+        <div className="max-w-sm border border-gray-700 rounded-lg bg-gray-800 text-gray-400">
+            <h2 className="text-white font-bold">{show.name}</h2>
             <p>{show.location}</p>
         </div>
     )
---
+++ tailwind.config.cjs
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+        "./index.html",
+        "./src/**/*.{js,ts,jsx,tsx}",
+        "node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}"
+    ],
+    theme: {
+        extend: {
+            fontFamily: {
+                sans: [
+                    "Montserrat"
+                ]
+            }
+        },
+    },
+    plugins: [require('flowbite/plugin')],
+}
