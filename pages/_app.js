f39a4d4161ed5685f2f94441702c55807e1f72e8
ruinaz90 <52875611+ruinaz90@users.noreply.github.com>
2023-03-09 19:35:28 -0500

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -14,8 +14,8 @@ export default function App({ Component, pageProps }) {
         <SessionContextProvider supabaseClient={supabaseClient} initialSession={pageProps.initialSession}>
         <Head>
             <title>Budget Broadway</title>
-            <meta name="keywords" content="Broadway, musicals, plays" />
-            <meta name="description" content="Information about discounted tickets for musicals and plays on Broadway" />
+            <meta name="keywords" content="Broadway, musicals, plays, discounts, lottery, rush" />
+            <meta name="description" content="Information about discounted tickets for Broadway musicals and plays" />
         </Head>
         
         <Layout>
