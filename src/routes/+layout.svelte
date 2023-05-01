d80851a23d3be33a36115beac5ac4df3f3f715a4
ruinaz90 <rzhao9002@gmail.com>
2023-05-01 07:15:30 -0400

    supabase auth

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,13 +2,30 @@
     import "../app.css"
     import Nav from "$lib/Nav.svelte"
     import Footer from "$lib/Footer.svelte"
+
+    import { invalidate } from '$app/navigation'
+	import { onMount } from 'svelte'
+
+    export let data
+
+	$: ({ supabase, session } = data)
+
+	onMount(() => {
+		const { data } = supabase.auth.onAuthStateChange((event, _session) => {
+			if (_session?.expires_at !== session?.expires_at) {
+				invalidate('supabase:auth')
+			}
+		})
+
+		return () => data.subscription.unsubscribe()
+	})
 </script>
 
 <Nav />
 <main class="mx-auto max-w-screen-xl lg:pt-20 lg:px-6">
     <!-- wrapper -->
     <div class="mt-12 lg:mt-8">
-        <slot></slot>
+        <slot />
     </div>
 </main>
 <Footer />
